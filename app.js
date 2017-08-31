d624d3870eb49025f3f2f8956b0ddef107a2c605
Syn <joel.gonzaga@gmail.com>
2017-08-31 12:01:57 -0700

    configured an output for address as well as latitude and longitude.

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ request({
   json: true
 }, function(error, response, body){
   if (body) {
-    console.log(body);
+    console.log("Street Address: ",body.results[0].formatted_address);
+    console.log("Latitude: ",body.results[0].geometry.location.lat);
+    console.log("Longitude: ",body.results[0].geometry.location.lng);
   };
   if (error) {
     console.log(error);
