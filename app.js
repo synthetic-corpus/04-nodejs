dd51beca1a0da9f5814398cff3b12b2d4fa8099d
Syn <joel.gonzaga@gmail.com>
2017-08-31 14:38:21 -0700

    Re-did the error handling.

--- app.js
+++ app.js
@@ -24,13 +24,17 @@ request({
   url: googleCoordinates,
   json: true
 }, function(error, response, body){
-  if (body) {
+  if (error) {
+    console.log(error);
+  }
+  else if (body.status === "ZERO_RESULTS"){
+    console.log("found no address like that");
+  }
+  else if (body.status === "OK") {
     console.log("Street Address: ",body.results[0].formatted_address);
     console.log("Latitude: ",body.results[0].geometry.location.lat);
     console.log("Longitude: ",body.results[0].geometry.location.lng);
   };
-  if (error) {
-    console.log(error);
-  };
+
 
 })
